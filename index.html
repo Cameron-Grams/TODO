52eee21b20bf179c24695b1664ebb19fb96901ac
Cameron <cameron.grams.2@gmail.com>
2017-06-16 14:14:57 -0400

    first commit

---
+++ index.html
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<title>Noter</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="styles.css" >
+</head>
+<body >
+
+<div>
+  <form>
+    <input type="text" id="note_text"
+       value="enter task" onfocus=" value = '' "
+       onkeypress="if ( event.keyCode === 13 ) createNote() " >
+
+    <input type="button" id="send_text" value="save" >
+
+    <input type="button" id="remove" onclick="removeNotes()" value="clear" >
+  </form>
+</div>
+
+<ul id="listOfNotes" >
+</ul>
+
+</body>
+<script src="logic.js" ></script>
+</html>
---
+++ logic.js
@@ -0,0 +1,72 @@
+window.onload = init;
+
+var $notes = document.getElementById( 'listOfNotes' ); 
+
+function init(){
+  let $button = document.getElementById( 'send_text' );
+  $button.onclick = createNote; 
+
+  for (let i = 0; i < localStorage.length; i++ ){
+    let key = localStorage.key( i );
+
+    if ( key.substring( 0, 4 ) === 'note' ){
+      let value = localStorage.getItem( key );
+      addToDOM(  key, value );
+    }
+  }
+};
+
+function addToDOM( key, value ){
+  let note = document.createElement( 'li' );
+  let span = document.createElement( 'span' );
+  let removeBtn = document.createElement( 'button' );
+
+  note.setAttribute( 'id', key );
+  span.setAttribute( 'class', 'note' );
+  span.innerHTML = value; 
+  removeBtn.innerHTML =  "X" ;
+  removeBtn.setAttribute( 'class', 'button' );
+  removeBtn.addEventListener( 'click', function( e ){
+    note.parentNode.removeChild( note );
+    localStorage.removeItem( key );
+  }, false ); 
+
+  note.appendChild( span );
+  note.appendChild( removeBtn );
+  $notes.appendChild( note );
+
+  console.log( localStorage.length );  
+};
+
+function manageIncrement(){
+  let nextValue = parseInt( localStorage[ "itr" ] );
+  nextValue += 1;
+  localStorage[ "itr" ] = nextValue; 
+  return nextValue;
+};
+
+function createNote(){
+
+  let entry = document.getElementById( 'note_text' ).value;
+  let key = "note_" + manageIncrement();
+  
+  localStorage.setItem( key, entry );
+  addToDOM( key, entry );
+  };
+
+function removeNotes( ){
+  localStorage.clear();
+  localStorage[ "itr" ] = 0;
+
+  while ( $notes.firstChild ){
+    $notes.removeChild( $notes.firstChild );
+  }
+};
+
+
+
+
+
+
+
+
